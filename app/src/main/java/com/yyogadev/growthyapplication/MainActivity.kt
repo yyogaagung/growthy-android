9a9a13ba5bf5deda3b6f839e68100f77b764af6f
valenciavannessa <119060608+valenciavannessa@users.noreply.github.com>
2023-05-12 15:51:04 +0700

    Menambahkan Prediction Activity

--- app/src/main/java/com/yyogadev/growthyapplication/MainActivity.kt
+++ app/src/main/java/com/yyogadev/growthyapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.yyogadev.growthyapplication
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,17 +20,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.fiturCamerabtn.setOnClickListener {
+            startFiturCamera()
+        }
+
+        binding.fiturPrediksibtn.setOnClickListener {
+            startFiturPrediksi()
+        }
+
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun startFiturPrediksi() {
+        val intent = Intent(this, PredictionActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startFiturCamera() {
+//        val intent = Intent(this, UploadActivity::class.java)
+//        startActivity(intent)
     }
 }
