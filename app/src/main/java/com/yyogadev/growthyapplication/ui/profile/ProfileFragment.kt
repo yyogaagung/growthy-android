46f1790ef34de89aecb669941a10f63cc93a78dc
yyogaagung <gcyogaagung123@gmail.com>
2023-06-05 21:58:42 +0700

    mengerjakan logout

--- app/src/main/java/com/yyogadev/growthyapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yyogadev/growthyapplication/ui/profile/ProfileFragment.kt
@@ -1,18 +1,31 @@
 package com.yyogadev.growthyapplication.ui.profile
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.camera.core.impl.utils.ContextUtil
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.yyogadev.growthyapplication.R
 import com.yyogadev.growthyapplication.databinding.FragmentProfileBinding
+import com.yyogadev.growthyapplication.ui.SettingPreferences
+import com.yyogadev.growthyapplication.ui.TokenViewModel
+import com.yyogadev.growthyapplication.ui.TokenViewModelFactory
+import com.yyogadev.growthyapplication.ui.login.LoginActivity
 
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class ProfileFragment : Fragment(),View.OnClickListener {
 
     private var _binding: FragmentProfileBinding?= null
+    private lateinit var tokenViewModel: TokenViewModel
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -34,7 +47,24 @@ class ProfileFragment : Fragment(),View.OnClickListener {
 //            textView.text = text
 //        }
         binding.toEdtProfile.setOnClickListener(this)
+        binding.btnLogout.setOnClickListener(this)
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val pref = SettingPreferences.getInstance(requireContext().dataStore)
+        tokenViewModel = ViewModelProvider(this, TokenViewModelFactory(pref)).get(
+            TokenViewModel::class.java
+        )
+
+        tokenViewModel.getToken().observe(this) { token: String->
+            if (token.isEmpty()) {
+                val i = Intent(this.requireContext(), LoginActivity::class.java)
+                startActivity(i)
+            }
+        }
     }
 
     override fun onClick(v: View) {
@@ -46,6 +76,9 @@ class ProfileFragment : Fragment(),View.OnClickListener {
                 addToBackStack(null)
                 commit()
             }
+        }
+        if(v.id == R.id.btn_logout){
+            tokenViewModel.saveToken("")
         }
     }
 
